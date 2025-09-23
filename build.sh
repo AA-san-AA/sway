68a83d30872e178e8418f7b902ab4863d8bdae4f
meiyongjia <meiyongjia@cdjrlc.com>
2025-09-23 11:35:34 +0800

    script: Add the cairo library to the script
    Signed-off-by: meiyongjia <meiyongjia@cdjrlc.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 rm -rf build &&
-meson setup -Dwlroots:default_library=static -Dwayland:default_library=static -Dwayland:documentation=false \
+meson setup -Dwlroots:default_library=static -Dwayland:default_library=static -Dwayland:documentation=false -Dcairo:default_library=static \
 -Dlibffi:default_library=static \
 -Dc_std=gnu11 \
---force-fallback-for=wayland,libffi \
--Dc_args="-Wno-error=deprecated-declarations -Wno-error=implicit-function-declaration" build \
+-Dc_args="-Wno-error " build \
+--force-fallback-for=wayland,cairo \
+-Dc_link_args=" -lfreetype -lxkbcommon -lcairo " \
 --prefix=${PWD}/sway_install &&
 cd build &&
 meson compile && meson install &&
